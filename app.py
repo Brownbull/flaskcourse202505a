ea77caecefac24f822427b64d0a812cc65b2ba2a
gabriel carcamo marambio <carcamo.gabriel@gmail.com>
2025-05-07 23:18:31 -0400

    2

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
   return "<h1>Hello, World!</h1>"
+
+@app.route("/home")
+def home():
+  return "<h1>Welcome to the Home Page!</h1>"
+
+@app.route("/json")
+def json_response():
+  return {
+    "message": ["Hello", "JSON!"],
+    "status": "success"
+  }
+
