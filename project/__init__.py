240c48342b11cd77e306dd66b489d9c56595dc1d
Gabe <carcamo.gabriel@gmail.com>
2025-05-09 18:59:23 -0400

    App Structure initial

--- project/__init__.py
+++ project/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
+from flask import Blueprint
 from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
     app = Flask(__name__)
+
+    main = Blueprint('main', __name__)
+    app.register_blueprint(main)
     
     @app.route('/')
     def index():
