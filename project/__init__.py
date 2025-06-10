47cec3fa4fc0b138fb2cb433a3343349e977d424
gabriel carcamo marambio <carcamo.gabriel@gmail.com>
2025-06-10 18:15:14 -0400

    blueprints start with database cration

--- project/__init__.py
+++ project/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 
+from .extensions import db
 from .routes import main
 
 def create_app():
-    app = Flask(__name__)
+  app = Flask(__name__)
 
-    app.register_blueprint(main)
-    
-    return app
+  app.config.from_prefixed_env()
+
+  db.init_app(app)
+
+  app.register_blueprint(main)
+  
+  return app
