4d7026351562003494dd1e4f461f4a9515643b01
gabriel carcamo marambio <carcamo.gabriel@gmail.com>
2025-06-12 18:38:55 -0400

    validation data handling in form added eror messages

--- registration_form/views.py
+++ registration_form/views.py
@@ -12,6 +12,8 @@ def index(member_id):
     member = None
     if member_id:
         member = Member.query.get_or_404(member_id)
+
+    errors = {}
     
     if request.method == 'POST':
         # Get input
@@ -24,40 +26,56 @@ def index(member_id):
         learn_new_interest = request.form['learn_new_interest']
         interest_in_topics = request.form.getlist('interest_in_topics')
 
-        if member:
-            # UDPATE existing member
-            member.email = email
-            if password:
-                member.password = password
-            member.location = location
-            member.first_learn_date = datetime.strptime(first_learn_date, '%Y-%m-%d') if first_learn_date else None
-            member.fav_language = fav_language
-            member.about = about
-            member.learn_new_interest = True if learn_new_interest == "Yes" else False
-            member.interest_in_topics[:] = []  # Clear existing topics
-        else:
-            # CREATE new member
-            member = Member(
-                email = email,
-                password = password,
-                location = location,
-                first_learn_date = datetime.strptime(first_learn_date, '%Y-%m-%d') if first_learn_date else None,
-                fav_language = fav_language,
-                about = about,
-                learn_new_interest = True if learn_new_interest == "Yes" else False,
-            )
+        # Validate input
+        if not email:
+            errors['email'] = 'Email is required.'
+        if not password and not member_id:  # Only require password if creating a new member
+            errors['password'] = 'Password is required.'
+        if not location:
+            errors['location'] = 'Location is required.'
+        if not first_learn_date:
+            errors['first_learn_date'] = 'First learn date is required.'
+        if not about:
+            errors['about'] = 'About section is required.'
+        if not interest_in_topics:
+            errors['interest_in_topics'] = 'At least one topic of interest is required.'
+        if not errors:
+            print('No errors')
 
-            db.session.add(member)
+            if member:
+                # UDPATE existing member
+                member.email = email
+                if password:
+                    member.password = password
+                member.location = location
+                member.first_learn_date = datetime.strptime(first_learn_date, '%Y-%m-%d') if first_learn_date else None
+                member.fav_language = fav_language
+                member.about = about
+                member.learn_new_interest = True if learn_new_interest == "Yes" else False
+                member.interest_in_topics[:] = []  # Clear existing topics
+            else:
+                # CREATE new member
+                member = Member(
+                    email = email,
+                    password = password,
+                    location = location,
+                    first_learn_date = datetime.strptime(first_learn_date, '%Y-%m-%d') if first_learn_date else None,
+                    fav_language = fav_language,
+                    about = about,
+                    learn_new_interest = True if learn_new_interest == "Yes" else False,
+                )
 
-        for topic_id in interest_in_topics:
-            topic = Topic.query.get(topic_id)
-            if topic:
-                member.interest_in_topics.append(topic)
+                db.session.add(member)
 
-        db.session.commit()
-        print('Member added to database:', member)
+            for topic_id in interest_in_topics:
+                topic = Topic.query.get(topic_id)
+                if topic:
+                    member.interest_in_topics.append(topic)
 
-        return redirect(url_for('main.index', member_id=member.id))
+            db.session.commit()
+            print('Member added to database:', member)
+
+            return redirect(url_for('main.index', member_id=member.id))
     
     languages = Language.query.all()
     topics = Topic.query.all()
@@ -66,7 +84,8 @@ def index(member_id):
         'member_id': member_id,
         'languages': languages,
         'topics': topics,
-        'member': member
+        'member': member,
+        'errors': errors
     }
 
     return render_template('form.html', **context)
