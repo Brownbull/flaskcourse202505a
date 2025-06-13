7ffb0a7f709c04038f70f71eebc75a179865cb7b
gabriel carcamo marambio <carcamo.gabriel@gmail.com>
2025-06-13 11:19:11 -0400

    api update route working

--- registration_form/views/api.py
+++ registration_form/views/api.py
@@ -49,5 +49,39 @@ def create_member():
         db.session.rollback()
         return jsonify({'error': str(e)}), 400
 
+@api.route('/member/<int:member_id>', methods=['PUT', 'PATCH'])
+def update_member(member_id):
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'No input data provided'}), 400
+    
+    member = Member.query.get_or_404(member_id)
+    # Update member attributes
+    member.email = data.get('email', member.email)
+    if data.get('password'):
+        # Only update password if provided
+        member.password = data.get('password')
+    member.location = data.get('location')
+    if data.get('first_learn_date'):
+        member.first_learn_date = datetime.strptime(
+            data.get('first_learn_date'), 
+            '%Y-%m-%d')
+    member.fav_language = data['fav_language'].get('id')
+    member.about = data.get('about')
+    member.learn_new_interest = data.get('learn_new_interest')
+
+    # Update member topics
+    topics = data.get('interest_in_topics', [])
+    member.interest_in_topics = []
+    for member_topic in topics:
+        topic = Topic.query.get(member_topic['id'])
+        member.interest_in_topics.append(topic)
+
+    try:
+        db.session.commit()
+        return jsonify({'member': member.to_json()}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': str(e)}), 400
 
     
