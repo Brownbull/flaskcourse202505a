03dd275ada05399d03024d0061ce94330bc452a2
gabriel carcamo marambio <carcamo.gabriel@gmail.com>
2025-06-17 18:15:28 -0400

    models created on dashboard app

---
+++ dashboard/models.py
@@ -0,0 +1,28 @@
+from .extensions import db
+
+class Customers(db.Model):
+    __tablename__ = 'customers'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    
+class Product(db.Model):
+    __tablename__ = 'products'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer)
+    goal = db.Column(db.Integer)
+    
+class Order(db.Model):
+    __tablename__ = 'orders'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    order_date = db.Column(db.DateTime, nullable=False)
+
+    # customer = db.relationship('Customers', backref=db.backref('orders', lazy=True))
+    # product = db.relationship('Product', backref=db.backref('orders', lazy=True))
+
