f27cbb5b14517bf40a757701fce5f2a1e8f23d6b
gabriel carcamo marambio <carcamo.gabriel@gmail.com>
2025-07-04 10:51:19 -0400

    register working on dashboard

--- dashboard/models.py
+++ dashboard/models.py
@@ -67,5 +67,6 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    name = db.Column(db.String(50))
+    email_address = db.Column(db.String(50))
+    password_hash = db.Column(db.String(50))
