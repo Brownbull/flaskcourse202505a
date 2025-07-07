dc1931da6a8ddc1e7a9d9c8e9917ae3215cf2336
gabriel carcamo marambio <carcamo.gabriel@gmail.com>
2025-07-07 12:46:06 -0400

    engage starting followeers stuff

--- engage/models.py
+++ engage/models.py
@@ -1,6 +1,10 @@
 from .extensions import db
 from flask_login import UserMixin
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
@@ -8,6 +12,15 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(120), nullable=False)
     image = db.Column(db.String(120), nullable=True)
     join_date = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+
+    following = db.relationship(
+        'User',
+        secondary = followers,
+        primaryjoin = (followers.c.follower_id == id),
+        secondaryjoin = (followers.c.followed_id == id),
+        backref = db.backref('followers', lazy='dynamic'),
+        lazy = 'dynamic'
+    )
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -22,3 +35,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.id} by {self.user.username}>'
+    
