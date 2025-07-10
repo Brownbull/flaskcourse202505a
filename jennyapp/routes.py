e7621e73f18260a80701fe7ad5c1bd7289b182bc
gabriel carcamo marambio <carcamo.gabriel@gmail.com>
2025-07-10 07:06:56 -0400

    added a los of html templates working

--- jennyapp/routes.py
+++ jennyapp/routes.py
@@ -17,3 +17,31 @@ def index():
 @main.route('/login')
 def login():
     return render_template('login.html')
+
+@main.route('/dashboard')
+def dashboard():
+    return render_template('dashboard/dashboard.html')
+
+@main.route('/profile')
+def profile():
+    return render_template('dashboard/profile.html')
+
+@main.route('/activity')
+def activity():
+    return render_template('dashboard/activity.html')
+
+@main.route('/patients')
+def patients():
+    return render_template('dashboard/patients.html')
+
+@main.route('/session')
+def session():
+    return render_template('dashboard/session.html')
+
+@main.route('/calendar')
+def calendar():
+    return render_template('dashboard/calendar.html')
+
+@main.route('/documents')
+def documents():
+    return render_template('dashboard/documents.html')
