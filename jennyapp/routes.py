9e69129488252354d46c55a2bf00cf2419f17338
gabriel carcamo marambio <carcamo.gabriel@gmail.com>
2025-07-10 07:25:22 -0400

    working on register

--- jennyapp/routes.py
+++ jennyapp/routes.py
@@ -14,6 +14,10 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
+@main.route('/register')
+def register():
+    return render_template('register.html')
+
 @main.route('/login')
 def login():
     return render_template('login.html')
