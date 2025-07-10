62557b2f85a1a8697ecfe52055276c225006e2ae
gabriel carcamo marambio <carcamo.gabriel@gmail.com>
2025-07-10 11:32:18 -0400

    updated Z template for login register

--- archive/Z01_base_tailwind/jennyapp/__init__.py
+++ archive/Z01_base_tailwind/jennyapp/__init__.py
@@ -3,8 +3,10 @@ from flask import Flask, url_for
 
 from .commands import create_tables
 from .extensions import db, migrate, login_manager
-from .routes import store
+from .routes import main
 from .utils import time_since
+
+from .models import User
 
 def create_app():
   app = Flask(__name__)
@@ -13,19 +15,19 @@ def create_app():
 
   db.init_app(app)
   migrate.init_app(app, db)
-  # login_manager.init_app(app)
+  login_manager.init_app(app)
 
-  # login_manager.login_view = 'store.index'
+  login_manager.login_view = 'main.index'
 
-  # @login_manager.user_loader
-  # def load_user(user_id):
-  #     return User.query.get(user_id)
+  @login_manager.user_loader
+  def load_user(user_id):
+      return User.query.get(user_id)
       
   # UTILS
   app.add_template_filter(time_since, 'time_since')
 
   # ROUTES
-  app.register_blueprint(store)
+  app.register_blueprint(main)
   
   # COMMANDS
   app.cli.add_command(create_tables)
--- archive/Z01_base_tailwind/jennyapp/routes.py
+++ archive/Z01_base_tailwind/jennyapp/routes.py
@@ -8,9 +8,103 @@ import os
 UPLOAD_FOLDER = 'static/uploads'
 
 from .extensions import db
+from .forms import RegisterForm, LoginForm
+from .models import User
 
-store = Blueprint('store', __name__)
-@store.route('/')
+main = Blueprint('main', __name__)
+@main.route('/')
 def index():
     return render_template('index.html')
 
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            error = "Email address already exists. Please use a different email."
+        else:
+            register_user = User(
+                email=form.email.data,
+                password=form.password.data,
+                join_date=datetime.now()
+            )
+            db.session.add(register_user)
+            db.session.commit()
+            login_user(register_user, remember=False) 
+            return redirect(url_for('main.dashboard'))
+        
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render_template('register.html', **context)
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    form = LoginForm()
+    
+    # if request.method == 'GET':
+    #     return render_template('login.html')
+
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if not user or not user.verify_password(form.password.data):
+            error = 'Invalid username or password'
+            # return render_template('login.html', form=form, error='Invalid username or password')
+        else:
+            login_user(user, remember=form.remember_me.data) 
+            return redirect(url_for('main.dashboard'))
+
+    if error:
+        print(f"error: {error}")
+    context = {
+        'form': form,
+        'error': error,
+    }
+    return render_template('login.html', **context)
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+@main.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard/dashboard.html')
+
+@main.route('/profile')
+@login_required
+def profile():
+    return render_template('dashboard/profile.html')
+
+@main.route('/activity')
+@login_required
+def activity():
+    return render_template('dashboard/activity.html')
+
+@main.route('/patients')
+@login_required
+def patients():
+    return render_template('dashboard/patients.html')
+
+@main.route('/session')
+@login_required
+def session():
+    return render_template('dashboard/session.html')
+
+@main.route('/calendar')
+@login_required
+def calendar():
+    return render_template('dashboard/calendar.html')
+
+@main.route('/documents')
+@login_required
+def documents():
+    return render_template('dashboard/documents.html')
